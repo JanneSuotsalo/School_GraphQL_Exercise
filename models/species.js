667d504401c74aca9e7af81ec6594e67b51c99da
Janne Suotsalo <janne.suotsalo@gmail.com>
2020-04-04 00:26:04 +0300

    added connection to db and mutations

---
+++ models/species.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const speciesSchema = new Schema({
+  speciesName: String,
+  category: { type: mongoose.Types.ObjectId, ref: "Category" }
+});
+
+module.exports = mongoose.model("Species", speciesSchema);
--- schema/schema.js
+++ schema/schema.js
@@ -7,6 +7,10 @@ const {
   GraphQLSchema
 } = require("graphql");
 
+const category = require("../models/category");
+const species = require("../models/species");
+const animal = require("../models/animal");
+
 const animalData = [
   {
     id: "1",
@@ -87,9 +91,10 @@ const RootQuery = new GraphQLObjectType({
       type: new GraphQLNonNull(GraphQLList(animalType)),
       description: "Get all animals",
       resolve: (parent, args) => {
-        return animalData;
+        return animal.find();
       }
     },
+
     animal: {
       type: animalType,
       description: "Get animal by id",
@@ -99,10 +104,73 @@ const RootQuery = new GraphQLObjectType({
       resolve: (parent, args) => {
         return animalData.find(animal => animal.id === args.id);
       }
+    },
+
+    categories: {
+      type: new GraphQLNonNull(GraphQLList(categoryType)),
+      description: "Get all categories",
+      resolve: async (parent, args) => {
+        return await category.find();
+      }
+    },
+
+    species: {
+      type: new GraphQLNonNull(GraphQLList(speciesType)),
+      description: "Get all species",
+      resolve: async (parent, args) => {
+        return await species.find();
+      }
+    }
+  }
+});
+
+const Mutation = new GraphQLObjectType({
+  name: "MutationType",
+  description: "Mutations...",
+  fields: {
+    addCategory: {
+      type: categoryType,
+      description: "Add animal category like Fish, Mammal, etc.",
+      args: {
+        categoryName: { type: new GraphQLNonNull(GraphQLString) }
+      },
+      resolve(parent, args) {
+        const newCategory = new category({
+          categoryName: args.categoryName
+        });
+        return newCategory.save();
+      }
+    },
+
+    addSpecies: {
+      type: speciesType,
+      description: "Add animal species like Cat, Dog, etc. and category id",
+      args: {
+        speciesName: { type: new GraphQLNonNull(GraphQLString) },
+        category: { type: new GraphQLNonNull(GraphQLID) }
+      },
+      resolve(parent, args) {
+        const newSpecies = new species(args);
+        return newSpecies.save();
+      }
+    },
+
+    addAnimal: {
+      type: animalType,
+      description: "Add animal like Frank, John, etc. and species id",
+      args: {
+        animalName: { type: new GraphQLNonNull(GraphQLString) },
+        species: { type: new GraphQLNonNull(GraphQLID) }
+      },
+      resolve(parent, args) {
+        const newAnimal = new animal(args);
+        return newAnimal.save();
+      }
     }
   }
 });
 
 module.exports = new GraphQLSchema({
-  query: RootQuery
+  query: RootQuery,
+  mutation: Mutation
 });
