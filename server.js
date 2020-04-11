bac62a31c3f123ed3d507f860fffa7734f23f4d9
Janne Suotsalo <janne.suotsalo@gmail.com>
2020-04-11 05:28:33 +0300

    added bcrypt

--- server.js
+++ server.js
@@ -12,14 +12,47 @@ const connectionTypeRoute = require("./routes/connectionTypeRoute");
 const currentTypeRoute = require("./routes/currentTypeRoute");
 const levelRoute = require("./routes/levelRoute");
 
+const authRoute = require("./routes/authRoute");
+const passport = require("./utils/pass");
+const cors = require("cors");
+//const bcrypt = require("bcrypt");
+//const saltRound = 12;
+
+app.use(cors());
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+// normally, that's the user create/update route
+/*app.get("/", async (req, res) => {
+  const hash = await bcrypt.hash("qwer", saltRound);
+  res.send("hash of password: " + hash);
+});*/
 
 app.use("/station", stationRoute);
 app.use("/connection", connectionRoute);
 app.use("/connectionType", connectionTypeRoute);
 app.use("/currentType", currentTypeRoute);
 app.use("/level", levelRoute);
+
+// dummy function to set user (irl: e.g. passport-local)
+const auth = (req, res, next) => {
+  req.user = false;
+  next();
+};
+
+// dummy function to check authentication (irl: e.g. passport-jwt)
+const checkAuth = (req, res) => {
+  passport.authenticate("jwt", { session: false }, (err, user) => {
+    if (err || !user) {
+      throw new Error("Not authenticated");
+    }
+  })(req, res);
+};
+
+//app.use(auth);
+//app.post(auth);
+
+app.use("/auth", authRoute);
 
 app.get("/test", async (req, res) => {
   if (req.secure) {
